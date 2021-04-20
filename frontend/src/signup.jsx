3d14bac08cf8a0188d64f78a648ca8ac1bd49129
Austin Liu <37671748+ostenloo@users.noreply.github.com>
2021-04-20 15:04:55 -0500

    Update signup.jsx

--- frontend/src/signup.jsx
+++ frontend/src/signup.jsx
@@ -1,7 +1,11 @@
 import React from 'react'; 
 import axios from 'axios';
+import { Repository } from '../api/repository';
+import {Link, Redirect} from 'react-router-dom';
 
 export class Signup extends React.Component {
+    
+    repo = new Repository(); 
     
     state = { 
         firstName: "", 
