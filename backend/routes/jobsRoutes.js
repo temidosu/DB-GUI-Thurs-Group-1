e415074848f0fbc288a12ff58bc4ce045e5afb2d
Adam <aczarnik@smu.edu>
2021-04-26 18:20:59 -0500

    working on api calls

--- backend/routes/jobsRoutes.js
+++ backend/routes/jobsRoutes.js
@@ -6,14 +6,14 @@ const fs = require('fs');
 const { json } = require('body-parser');
 
 // Get all jobs
-app.get('/jobs', (req, res) => {
+app.get('/projects', (req, res) => {
 	pool.getConnection((err, connection) => {
 		if (err) {
 			console.log(connection);
 			logger.error('Problem obtaining MySQL connection', err)
 			res.status(400).send('Problem obtaining MySQL connection');
 		} else {
-			connection.query("SELECT * FROM Project_Requests", function (err, result, fields) {
+			connection.query("SELECT * FROM Projects", function (err, result, fields) {
 				if (err) {
 					logger.error('', err);
 					res.status(400).send('failed');
@@ -26,15 +26,36 @@ app.get('/jobs', (req, res) => {
 	})
 });
 
-// Get only ongoing jobs
-app.get('/jobs/ongoing', (req, res) => {
+// Get only in progress jobs
+app.get('/projects/inprogress', (req, res) => {
 	pool.getConnection((err, connection) => {
 		if (err) {
 			console.log(connection);
 			logger.error('Problem obtaining MySQL connection', err)
 			res.status(400).send('Problem obtaining MySQL connection');
 		} else {
-			connection.query("SELECT * FROM Project_Requests WHERE Status = 'Ongoing'", function (err, result, fields) {
+			connection.query("SELECT * FROM Projects WHERE Status = 'In Progress'", function (err, result, fields) {
+				if (err) {
+					logger.error('', err);
+					res.status(400).send('failed');
+				}
+				else {
+					res.status(200).json(JSON.parse(JSON.stringify(result)))
+				}
+			});
+		}
+	})
+});
+
+// Get only pending jobs
+app.get('/projects/pending', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			console.log(connection);
+			logger.error('Problem obtaining MySQL connection', err)
+			res.status(400).send('Problem obtaining MySQL connection');
+		} else {
+			connection.query("SELECT * FROM Projects WHERE Status = 'Pending'", function (err, result, fields) {
 				if (err) {
 					logger.error('', err);
 					res.status(400).send('failed');
@@ -48,14 +69,14 @@ app.get('/jobs/ongoing', (req, res) => {
 });
 
 // Get only completed jobs
-app.get('/jobs/completed', (req, res) => {
+app.get('/projects/completed', (req, res) => {
 	pool.getConnection((err, connection) => {
 		if (err) {
 			console.log(connection);
 			logger.error('Problem obtaining MySQL connection', err)
 			res.status(400).send('Problem obtaining MySQL connection');
 		} else {
-			connection.query("SELECT * FROM Project_Requests WHERE Status = 'Completed'", function (err, result, fields) {
+			connection.query("SELECT * FROM Projects WHERE Status = 'Completed'", function (err, result, fields) {
 				if (err) {
 					logger.error('', err);
 					res.status(400).send('failed');
@@ -70,14 +91,56 @@ app.get('/jobs/completed', (req, res) => {
 
 
 // Get only cancelled jobs
-app.get('/jobs/cancelled', (req, res) => {
+app.get('/projects/cancelled', (req, res) => {
 	pool.getConnection((err, connection) => {
 		if (err) {
 			console.log(connection);
 			logger.error('Problem obtaining MySQL connection', err)
 			res.status(400).send('Problem obtaining MySQL connection');
 		} else {
-			connection.query("SELECT * FROM Project_Requests WHERE Status = 'Cancelled'", function (err, result, fields) {
+			connection.query("SELECT * FROM Projects WHERE Status = 'Cancelled'", function (err, result, fields) {
+				if (err) {
+					logger.error('', err);
+					res.status(400).send('failed');
+				}
+				else {
+					res.status(200).json(JSON.parse(JSON.stringify(result)))
+				}
+			});
+		}
+	})
+});
+
+// Get jobs need contractor
+app.get('/projects/needcontractor', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			console.log(connection);
+			logger.error('Problem obtaining MySQL connection', err)
+			res.status(400).send('Problem obtaining MySQL connection');
+		} else {
+			connection.query("SELECT * FROM Projects WHERE ContractorID IS NULL", function (err, result, fields) {
+				if (err) {
+					logger.error('', err);
+					res.status(400).send('failed');
+				}
+				else {
+					res.status(200).json(JSON.parse(JSON.stringify(result)))
+				}
+			});
+		}
+	})
+});
+
+// Get jobs need client
+app.get('/projects/needclient', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			console.log(connection);
+			logger.error('Problem obtaining MySQL connection', err)
+			res.status(400).send('Problem obtaining MySQL connection');
+		} else {
+			connection.query("SELECT * FROM Projects WHERE ClientID IS NULL", function (err, result, fields) {
 				if (err) {
 					logger.error('', err);
 					res.status(400).send('failed');
--- backend/routes/projectsRoutes.js
+++ backend/routes/projectsRoutes.js
@@ -5,26 +5,26 @@ const path = require('path')
 const fs = require('fs');
 const { json } = require('body-parser');
 
-// GET all projects
-app.get('/projects/all/', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if (err) {
-            console.log(connection);
-            logger.error('Problem obtaining MySQL connection', err)
-            res.status(400).send('Problem obtaining MySQL connection');
-        } else {
-            connection.query("SELECT * FROM Projects JOIN Users ON Projects.clientID = Users.user_id", function (err, result, fields) {
-                if (err) {
-                    logger.error('', err);
-                    res.status(400).send('failed');
-                }
-                else {
-                    res.status(200).json(JSON.parse(JSON.stringify(result))); 
-                }
-            });
-        }
-    })
-});
+// // GET all projects
+// app.get('/projects/all/', (req, res) => {
+//     pool.getConnection((err, connection) => {
+//         if (err) {
+//             console.log(connection);
+//             logger.error('Problem obtaining MySQL connection', err)
+//             res.status(400).send('Problem obtaining MySQL connection');
+//         } else {
+//             connection.query("SELECT * FROM Projects JOIN Users ON Projects.clientID = Users.user_id", function (err, result, fields) {
+//                 if (err) {
+//                     logger.error('', err);
+//                     res.status(400).send('failed');
+//                 }
+//                 else {
+//                     res.status(200).json(JSON.parse(JSON.stringify(result))); 
+//                 }
+//             });
+//         }
+//     })
+// });
 
 //GET project by ClientID
 app.get('/projects/clients/:ClientID', (req, res) => {
@@ -91,4 +91,27 @@ app.get('/projects/:job_id', (req, res) => {
         }
     })
 });
+
+//Get All Workers For a Given Project
+app.get('/projects/workers/:ProjectID', (req, res) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(connection);
+        logger.error('Problem obtaining MySQL connection', err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        var ProjectID = req.params.ProjectID
+        connection.query("SELECT * FROM Users JOIN Workers ON Users.user_id = Workers.userID WHERE role_id = 2 AND Workers.job_id = ?", ProjectID, function (err, result, fields) {
+          if (err) {
+            logger.error('', err);
+            res.status(400).send('failed');
+          }
+          else {
+            res.status(200).json(JSON.parse(JSON.stringify(result)))
+          }
+        });
+      }
+    })
+  });
+
 module.exports = app;
