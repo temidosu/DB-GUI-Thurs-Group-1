677dcf3abf87c1e0c0af59e54601d3c87b6583f8
Adam <aczarnik@smu.edu>
2021-04-26 18:15:15 -0500

    more api calls

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -167,6 +167,28 @@ app.get('/contractors', (req, res) => {
   })
 });
 
+//Get Available Contractors
+app.get('/contractors/available', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(connection);
+      logger.error('Problem obtaining MySQL connection', err)
+      res.status(400).send('Problem obtaining MySQL connection');
+    } else {
+      connection.query("SELECT * FROM Users JOIN Projects ON Users.user_id = Projects.ContractorID WHERE role_id = 3", function (err, result, fields) {
+        if (err) {
+          logger.error('', err);
+          res.status(400).send('failed');
+        }
+        else {
+          res.status(200).json(JSON.parse(JSON.stringify(result)))
+        }
+      });
+    }
+  })
+});
+
+//Get user info for given ID
 app.get('/userInfo/:userID', (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) {
